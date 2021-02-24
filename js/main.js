13fbfbec4fc6a81655a1b933eba729564ff83261
AndreyBob <54039246+AndreyBob@users.noreply.github.com>
2021-02-24 19:38:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,10 +42,29 @@ function checkAddress() {
 }
 
 function onNextClick() {
-    let firstName = document.getElementById("first_name").value;
-    let lastName = document.getElementById("last_name").value;
-    document.querySelector(".div-firstname").innerHTML = firstName;
-    document.querySelector(".div-lastname").innerHTML = lastName;
+    let company = document.getElementById("company").value;
+  let firstName = document.getElementById("first_name").value;
+  let lastName = document.getElementById("last_name").value;
+  let site = document.getElementById("url").value;
+  let birthday = document.getElementById("00N5g000003wq0B").value;
+  let email = document.getElementById("email").value;
+  let billcountry = document.getElementById("country").value;
+  let billstate = document.getElementById("state").value;
+  let billzip = document.getElementById("zip").value;
+  let billcity = document.getElementById("city").value;
+  let billstreet = document.getElementById("street").value;
+
+  document.querySelector(".div-url").innerHTML = site;
+  document.querySelector(".div-company").innerHTML = company;
+  document.querySelector(".div-firstname").innerHTML = firstName;
+  document.querySelector(".div-lastname").innerHTML = lastName;
+  document.querySelector(".div-birthday").innerHTML = birthday;
+  document.querySelector(".div-email").innerHTML = email;
+  document.querySelector(".div-billcountry").innerHTML = billcountry;
+  document.querySelector(".div-billstate").innerHTML = billstate;
+  document.querySelector(".div-billzip").innerHTML = billzip;
+  document.querySelector(".div-billcity").innerHTML = billcity;
+  document.querySelector(".div-billstreet").innerHTML = billstreet;
 }
 
 function callValidation(){
